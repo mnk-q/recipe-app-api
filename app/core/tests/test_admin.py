be286dbc00bb179670df25643d266d5a196442ad
Manish Pandey <spmv.2006@gmail.com>
2021-03-27 17:19:23 +0530

    corrected code formatting according to pep8

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -2,6 +2,7 @@ from django.test import TestCase, Client
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 
+
 class AdminSiteTests(TestCase):
 
     def setUp(self):
@@ -32,7 +33,7 @@ class AdminSiteTests(TestCase):
         res = self.client.get(url)
 
         self.assertEqual(res.status_code, 200)
-    
+
     def test_create_user_page(self):
         """ Test whether user create page works"""
         url = reverse('admin:core_user_add')
